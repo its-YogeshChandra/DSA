bbc9e443ab624e350cd0a19f0bed796e8ad7e726
its-YogeshChandra <pandittheroyal@gmail.com>
2026-06-09 22:01:02 +0530

    adding: palindrome question

---
+++ dataStructures/String/src/bin/check_palindrome.rs
@@ -0,0 +1,17 @@
+// Palindrome String
+// Last Updated : 8 Apr, 2026
+// Given a string s, the task is to check if it is palindrome or not.
+//
+// Example:
+//
+// Input: s = "abba"
+// Output: true
+// Explanation: s is a palindrome
+//
+// Input: s = "abc"
+// Output: false
+// Explanation: s is not a palindromen
+
+fn main() {}
+
+fn check_palindrome(main_str: &str) {}
