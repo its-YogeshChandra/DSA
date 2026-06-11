8f4dd7713ce7e58d7e4745cad62dbb4792f2bf13
its-YogeshChandra <pandittheroyal@gmail.com>
2026-06-11 15:38:00 +0530

    feat: added check palindrome(kind of optimal approach)

--- dataStructures/String/src/bin/check_palindrome.rs
+++ dataStructures/String/src/bin/check_palindrome.rs
@@ -12,6 +12,24 @@
 // Output: false
 // Explanation: s is not a palindromen
 
-fn main() {}
+fn main() {
+    let str = "abc";
+    let result = check_palindrome(str);
+    println!("the result is : {}", result)
+}
 
-fn check_palindrome(main_str: &str) {}
+//using two pointers
+fn check_palindrome(main_str: &str) -> bool {
+    let mut i = 0;
+    let mut j = main_str.len() - 1;
+    let str_arr: Vec<char> = main_str.chars().collect();
+
+    while i < j {
+        if str_arr[i] != str_arr[j] {
+            return false;
+        }
+        i += 1;
+        j -= 1;
+    }
+    true
+}
