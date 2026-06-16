24df36f6746f017b60688533572fe860137c4d99
prxdotsol <thegreatcentauri@gmail.com>
2026-06-16 15:54:47 +0530

    adding if rotation question

---
+++ dataStructures/String/src/bin/check_ifrotation.rs
@@ -0,0 +1,24 @@
+// Check if Strings Are Rotations of Each Other
+// Last Updated : 3 Oct, 2025
+// Given two strings s1 and s2 of equal length, determine whether s2 is a rotation of s1.
+// A string is said to be a rotation of another if it can be obtained by shifting some leading characters of the original string to its end without changing the order of characters.
+//
+// Examples:
+//
+// Input: s1 = "abcd", s2 = "cdab"
+// Output: true
+// Explanation: After 2 right rotations, s1 will become equal to s2.
+//
+// Input: s1 = "aab", s2 = "aba"
+// Output: true
+// Explanation: After 1 left rotation, s1 will become equal to s2.
+//
+// Input: s1 = "abcd", s2 = "acbd"
+// Output: false
+// Explanation: Strings are not rotations of each other.
+
+fn main() {}
+
+fn check_ifrotation(s1: &str, s2: &str) -> bool {
+    true
+}
